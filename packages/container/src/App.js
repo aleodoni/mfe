586bdb4fcc868e4e3d9294e691a4dfd994931805
aleodoni <aleodoni@gmail.com>
2021-11-05 10:41:40 -0300

    Update container h1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 const App = () => {
   return (
     <div>
-      <h1>Hi there !!!!</h1>
+      <h1>Hi there !</h1>
       <hr />
       <MarketingApp />
     </div>
